d25a06157a96c3bbafe75a7097504360a39407a5
Anas Juwaidi <anas.didi95@gmail.com>
2020-08-21 20:56:06 +0800

    Add test

---
+++ src/test/java/com/anasdidi/api/user/TestUserVerticle.java
@@ -0,0 +1,46 @@
+package com.anasdidi.api.user;
+
+import com.anasdidi.security.MainVerticle;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import io.vertx.core.json.JsonObject;
+import io.vertx.junit5.VertxExtension;
+import io.vertx.junit5.VertxTestContext;
+import io.vertx.rxjava.core.Vertx;
+import io.vertx.rxjava.ext.web.client.WebClient;
+
+@ExtendWith(VertxExtension.class)
+public class TestUserVerticle {
+
+  private WebClient webClient;
+
+  @BeforeEach
+  void deploy_verticle(Vertx vertx, VertxTestContext testContext) {
+    webClient = WebClient.create(vertx);
+    vertx.deployVerticle(new MainVerticle(), testContext.succeeding(id -> testContext.completeNow()));
+  }
+
+  @Test
+  void testCanRouteToUser(Vertx vertx, VertxTestContext testContext) {
+    webClient.get(5000, "localhost", "/api/user").rxSend().subscribe(response -> {
+      testContext.verify(() -> {
+        Assertions.assertEquals(200, response.statusCode());
+        Assertions.assertEquals("application/json", response.getHeader("Accept"));
+        Assertions.assertEquals("application/json", response.getHeader("Content-Type"));
+
+        JsonObject responseBody = response.bodyAsJsonObject();
+        Assertions.assertNotNull(responseBody);
+
+        String body = responseBody.getString("data");
+        Assertions.assertEquals("Hello world", body);
+
+        testContext.completeNow();
+      });
+    }, e -> testContext.failNow(e));
+  }
+
+}

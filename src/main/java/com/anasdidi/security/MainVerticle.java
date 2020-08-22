33cd746dfc928658c6dbcd1e6a9b532ef47e9af2
Anas Juwaidi <anas.didi95@gmail.com>
2020-08-22 13:15:40 +0800

    Add users collection configuration

--- src/main/java/com/anasdidi/security/MainVerticle.java
+++ src/main/java/com/anasdidi/security/MainVerticle.java
@@ -3,16 +3,26 @@ package com.anasdidi.security;
 import com.anasdidi.security.api.user.UserVerticle;
 
 import io.vertx.core.Promise;
+import io.vertx.core.json.JsonObject;
 import io.vertx.reactivex.core.AbstractVerticle;
+import io.vertx.reactivex.ext.mongo.MongoClient;
 import io.vertx.reactivex.ext.web.Router;
 
 public class MainVerticle extends AbstractVerticle {
 
   @Override
   public void start(Promise<Void> startPromise) throws Exception {
+    MongoClient mongoClient = MongoClient.createShared(vertx, new JsonObject()//
+        .put("host", "mongo")//
+        .put("port", 27017)//
+        .put("username", "mongo")//
+        .put("password", "mongo")//
+        .put("authSource", "admin")//
+        .put("db_name", "security"));
+
     Router router = Router.router(vertx);
 
-    vertx.deployVerticle(new UserVerticle(router));
+    vertx.deployVerticle(new UserVerticle(router, mongoClient));
 
     vertx.createHttpServer().requestHandler(router).listen(5000, "localhost", http -> {
       if (http.succeeded()) {
--- src/main/java/com/anasdidi/security/api/user/UserVerticle.java
+++ src/main/java/com/anasdidi/security/api/user/UserVerticle.java
@@ -1,21 +1,50 @@
 package com.anasdidi.security.api.user;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import io.vertx.core.Promise;
 import io.vertx.core.json.JsonObject;
 import io.vertx.reactivex.core.AbstractVerticle;
+import io.vertx.reactivex.ext.mongo.MongoClient;
 import io.vertx.reactivex.ext.web.Router;
 import io.vertx.reactivex.ext.web.RoutingContext;
 
 public class UserVerticle extends AbstractVerticle {
 
   private final Router mainRouter;
+  private final MongoClient mongoClient;
 
-  public UserVerticle(Router mainRouter) {
+  public UserVerticle(Router mainRouter, MongoClient mongoClient) {
     this.mainRouter = mainRouter;
+    this.mongoClient = mongoClient;
   }
 
   @Override
   public void start(Promise<Void> startPromise) throws Exception {
+    mongoClient.getCollections(collectionList -> {
+      if (collectionList.succeeded()) {
+        String collectionName = "users";
+        List<String> resultList = collectionList.result().stream()//
+            .filter(collection -> collection.equals(collectionName))//
+            .collect(Collectors.toList());
+
+        if (resultList.isEmpty()) {
+          mongoClient.createCollection(collectionName, (result) -> {
+            if (result.succeeded()) {
+              System.out.println("[UserVerticle:start] Mongo create collection 'users' succeed.");
+            } else {
+              System.err.println("[UserVerticle:start] Mongo create collection 'users' failed.");
+              startPromise.fail(result.cause());
+            }
+          });
+        }
+      } else {
+        System.err.println("[UserVerticle:start] Mongo get collection list failed!");
+        startPromise.fail(collectionList.cause());
+      }
+    });
+
     Router router = Router.router(vertx);
     router.post("/").handler(this::create);
 

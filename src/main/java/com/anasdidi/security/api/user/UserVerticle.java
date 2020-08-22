f699357ed6cbb82dbd85834caa12b5fbe6d62894
Anas Juwaidi <anas.didi95@gmail.com>
2020-08-22 19:00:11 +0800

    Refactor configure mongo collection

--- src/main/java/com/anasdidi/security/api/user/UserVerticle.java
+++ src/main/java/com/anasdidi/security/api/user/UserVerticle.java
@@ -26,9 +26,22 @@ public class UserVerticle extends AbstractVerticle {
 
   @Override
   public void start(Promise<Void> startPromise) throws Exception {
+    configureMongoCollection(startPromise);
+
+    Router router = Router.router(vertx);
+    router.post("/").handler(userController::create);
+
+    mainRouter.mountSubRouter("/api/users", router);
+
+    System.out.println("[UserVerticle:start] Deployed success");
+    startPromise.complete();
+  }
+
+  void configureMongoCollection(Promise<Void> startPromise) {
+    String collectionName = "users";
+
     mongoClient.getCollections(collectionList -> {
       if (collectionList.succeeded()) {
-        String collectionName = "users";
         List<String> resultList = collectionList.result().stream()//
             .filter(collection -> collection.equals(collectionName))//
             .collect(Collectors.toList());
@@ -42,38 +55,34 @@ public class UserVerticle extends AbstractVerticle {
               startPromise.fail(result.cause());
             }
           });
-
-          mongoClient.listIndexes(collectionName, indexList -> {
-            if (indexList.succeeded()) {
-              @SuppressWarnings({ "unchecked" })
-              Set<String> indexSet = new HashSet<>(indexList.result().getList());
-
-              if (!indexSet.contains("idx_username")) {
-                mongoClient.rxCreateIndexWithOptions(//
-                    collectionName, //
-                    new JsonObject().put("username", 1), //
-                    new IndexOptions().name("idx_username").unique(true))//
-                    .subscribe();
-                System.out.println("[UserVerticle:start] Mongo create index 'idx_username' succeed.");
-              }
-            } else {
-              System.err.println("[UserVerticle:start] Mongo get index list failed!");
-              startPromise.fail(collectionList.cause());
-            }
-          });
         }
+
+        configureMongoCollectionIndexes(startPromise, collectionName);
       } else {
         System.err.println("[UserVerticle:start] Mongo get collection list failed!");
         startPromise.fail(collectionList.cause());
       }
     });
+  }
 
-    Router router = Router.router(vertx);
-    router.post("/").handler(userController::create);
+  void configureMongoCollectionIndexes(Promise<Void> startPromise, String collectionName) {
+    mongoClient.listIndexes(collectionName, indexList -> {
+      if (indexList.succeeded()) {
+        @SuppressWarnings({ "unchecked" })
+        Set<String> indexSet = new HashSet<>(indexList.result().getList());
 
-    mainRouter.mountSubRouter("/api/users", router);
-
-    System.out.println("[UserVerticle:start] Deployed success");
-    startPromise.complete();
+        if (!indexSet.contains("idx_username")) {
+          mongoClient.rxCreateIndexWithOptions(//
+              collectionName, //
+              new JsonObject().put("username", 1), //
+              new IndexOptions().name("idx_username").unique(true))//
+              .subscribe();
+          System.out.println("[UserVerticle:start] Mongo create index 'idx_username' succeed.");
+        }
+      } else {
+        System.err.println("[UserVerticle:start] Mongo get index list failed!");
+        startPromise.fail(indexList.cause());
+      }
+    });
   }
 }

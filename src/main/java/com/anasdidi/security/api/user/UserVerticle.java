471b7656ed78fc2b1708f87a467781f830032e87
Anas Juwaidi <anas.didi95@gmail.com>
2020-08-22 12:32:23 +0800

    Add route

--- src/main/java/com/anasdidi/security/api/user/UserVerticle.java
+++ src/main/java/com/anasdidi/security/api/user/UserVerticle.java
@@ -4,6 +4,7 @@ import io.vertx.core.Promise;
 import io.vertx.core.json.JsonObject;
 import io.vertx.reactivex.core.AbstractVerticle;
 import io.vertx.reactivex.ext.web.Router;
+import io.vertx.reactivex.ext.web.RoutingContext;
 
 public class UserVerticle extends AbstractVerticle {
 
@@ -16,17 +17,20 @@ public class UserVerticle extends AbstractVerticle {
   @Override
   public void start(Promise<Void> startPromise) throws Exception {
     Router router = Router.router(vertx);
-    router.get("/").handler(routingContext -> {
-      routingContext.response()//
-          .putHeader("Accept", "application/json")//
-          .putHeader("Content-Type", "application/json")//
-          .end(new JsonObject()//
-              .put("data", "Hello world").encode());
-    });
+    router.post("/").handler(this::create);
 
     mainRouter.mountSubRouter("/api/users", router);
 
     System.out.println("[UserVerticle:start] Deployed success");
     startPromise.complete();
+  }
+
+  void create(RoutingContext routingContext) {
+    routingContext.response()//
+        .putHeader("Accept", "application/json")//
+        .putHeader("Content-Type", "application/json")//
+        .setStatusCode(201)//
+        .end(new JsonObject()//
+            .put("data", "Hello world").encode());
   }
 }

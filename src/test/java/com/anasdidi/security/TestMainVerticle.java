8c08a21cd600ba6a33776859db32fc8c2adf6210
Anas Juwaidi <anas.didi95@gmail.com>
2020-08-22 12:57:16 +0800

    Configure mongo

--- src/test/java/com/anasdidi/security/TestMainVerticle.java
+++ src/test/java/com/anasdidi/security/TestMainVerticle.java
@@ -1,8 +1,11 @@
 package com.anasdidi.security;
 
-import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
 import io.vertx.junit5.VertxExtension;
 import io.vertx.junit5.VertxTestContext;
+import io.vertx.reactivex.core.Vertx;
+import io.vertx.reactivex.ext.mongo.MongoClient;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -18,5 +21,23 @@ public class TestMainVerticle {
   @Test
   void verticle_deployed(Vertx vertx, VertxTestContext testContext) throws Throwable {
     testContext.completeNow();
+  }
+
+  @Test
+  void testMongoConfigureSuccess(Vertx vertx, VertxTestContext testContext) {
+    String collectionName = "TestCollection";
+    MongoClient mongoClient = MongoClient.createShared(vertx, new JsonObject()//
+        .put("host", "mongo")//
+        .put("port", 27017)//
+        .put("username", "mongo")//
+        .put("password", "mongo")//
+        .put("authSource", "admin")//
+        .put("db_name", "security"));
+
+    mongoClient.rxCreateCollection(collectionName).subscribe(() -> {
+      mongoClient.rxDropCollection(collectionName).subscribe(() -> {
+        testContext.completeNow();
+      }, e -> testContext.failNow(e));
+    }, e -> testContext.failNow(e));
   }
 }

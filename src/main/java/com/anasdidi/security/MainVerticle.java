fc6c1344f1f93ebd64b90202722864488bb27375
Anas Juwaidi <anas.didi95@gmail.com>
2021-06-16 21:03:26 +0800

    Update file to use rxjava3 and test run

--- src/main/java/com/anasdidi/security/MainVerticle.java
+++ src/main/java/com/anasdidi/security/MainVerticle.java
@@ -1,23 +1,19 @@
 package com.anasdidi.security;
 
-import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
+import io.vertx.rxjava3.core.AbstractVerticle;
 
 public class MainVerticle extends AbstractVerticle {
 
   @Override
   public void start(Promise<Void> startPromise) throws Exception {
     vertx.createHttpServer().requestHandler(req -> {
-      req.response()
-        .putHeader("content-type", "text/plain")
-        .end("Hello from Vert.x!");
-    }).listen(8888, http -> {
-      if (http.succeeded()) {
-        startPromise.complete();
-        System.out.println("HTTP server started on port 8888");
-      } else {
-        startPromise.fail(http.cause());
-      }
+      req.response().putHeader("content-type", "text/plain").end("Hello from Vert.x!");
+    }).listen(5000).subscribe(server -> {
+      System.out.println("HTTP server started on port 5000");
+      startPromise.complete();
+    }, error -> {
+      startPromise.fail(error);
     });
   }
 }
--- src/test/java/com/anasdidi/security/TestMainVerticle.java
+++ src/test/java/com/anasdidi/security/TestMainVerticle.java
@@ -1,22 +1,28 @@
 package com.anasdidi.security;
 
-import io.vertx.core.Vertx;
-import io.vertx.junit5.VertxExtension;
-import io.vertx.junit5.VertxTestContext;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import io.vertx.junit5.VertxExtension;
+import io.vertx.junit5.VertxTestContext;
+import io.vertx.rxjava3.core.Vertx;
 
 @ExtendWith(VertxExtension.class)
 public class TestMainVerticle {
 
   @BeforeEach
-  void deploy_verticle(Vertx vertx, VertxTestContext testContext) {
-    vertx.deployVerticle(new MainVerticle(), testContext.succeeding(id -> testContext.completeNow()));
+  void deployVerticle(Vertx vertx, VertxTestContext testContext) {
+    vertx.deployVerticle(new MainVerticle()).subscribe(id -> {
+      testContext.verify(() -> {
+        Assertions.assertNotNull(id);
+        testContext.completeNow();
+      });
+    });
   }
 
   @Test
-  void verticle_deployed(Vertx vertx, VertxTestContext testContext) throws Throwable {
+  void testVerticleDeployed(Vertx vertx, VertxTestContext testContext) throws Throwable {
     testContext.completeNow();
   }
 }

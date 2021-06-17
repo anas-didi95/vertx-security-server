8f1d7c68f05b5264c267e7f76036d56161159d0f
Anas Juwaidi <anas.didi95@gmail.com>
2021-06-17 17:00:20 +0800

    Refactor code

--- src/main/java/com/anasdidi/security/MainVerticle.java
+++ src/main/java/com/anasdidi/security/MainVerticle.java
@@ -2,42 +2,35 @@ package com.anasdidi.security;
 
 import java.util.ArrayList;
 import java.util.List;
-import io.reactivex.rxjava3.core.Completable;
+import com.anasdidi.security.domain.mongo.MongoVerticle;
+import io.reactivex.rxjava3.core.Single;
 import io.vertx.core.Promise;
-import io.vertx.core.json.JsonObject;
+import io.vertx.core.Verticle;
 import io.vertx.rxjava3.core.AbstractVerticle;
-import io.vertx.rxjava3.ext.mongo.MongoClient;
 
 public class MainVerticle extends AbstractVerticle {
 
   @Override
-  public void start(Promise<Void> startPromise) throws Exception {
-    getMongoClient("mongodb://mongo:mongo@mongo:27017/security?authSource=admin");
-    vertx.createHttpServer().requestHandler(req -> {
-      req.response().putHeader("content-type", "text/plain").end("Hello from Vert.x!");
-    }).listen(5000).subscribe(server -> {
-      System.out.println("HTTP server started on port 5000");
-      startPromise.complete();
-    }, error -> {
-      startPromise.fail(error);
-    });
+  public void start(Promise<Void> startFuture) throws Exception {
+    List<Single<String>> deployer = new ArrayList<>();
+    deployer.add(deployVerticle(new MongoVerticle()));
+
+    Single.mergeDelayError(deployer).toList().subscribe(verticleList -> {
+      System.out.println("[start] Total deployed verticle: " + verticleList.size());
+      vertx.createHttpServer().requestHandler(req -> {
+        req.response().putHeader("content-type", "text/plain").end("Hello from Vert.x!");
+      }).listen(5000).subscribe(server -> {
+        System.out.println("HTTP server started on port 5000");
+        startFuture.complete();
+      }, error -> startFuture.fail(error));
+    }, error -> startFuture.fail(error));
   }
 
-  private MongoClient getMongoClient(String connectionString) {
-    MongoClient mongoClient = MongoClient.create(vertx, new JsonObject()//
-        .put("connection_string", connectionString));
-
-    mongoClient.rxGetCollections().subscribe(collectionList -> {
-      List<Completable> createCollection = new ArrayList<>();
-      if (!collectionList.contains("users")) {
-        createCollection.add(mongoClient.rxCreateCollection("users"));
-      }
-
-      if (!createCollection.isEmpty()) {
-        Completable.merge(createCollection).subscribe();
-      }
-    });
-
-    return mongoClient;
+  private Single<String> deployVerticle(Verticle verticle) {
+    return vertx.rxDeployVerticle(verticle)
+        .doOnSuccess(id -> System.out
+            .println("[deployVerticle] " + verticle.getClass().getName() + " OK: " + id))
+        .doOnError(error -> System.err
+            .println("[deployVerticle] " + verticle.getClass().getName() + " FAILED! " + error));
   }
 }

efb735920779927c226790cdfb44170cfebca083
Anas Juwaidi <anas.didi95@gmail.com>
2021-06-17 16:21:07 +0800

    Add implementation

--- src/main/java/com/anasdidi/security/MainVerticle.java
+++ src/main/java/com/anasdidi/security/MainVerticle.java
@@ -1,12 +1,18 @@
 package com.anasdidi.security;
 
+import java.util.ArrayList;
+import java.util.List;
+import io.reactivex.rxjava3.core.Completable;
 import io.vertx.core.Promise;
+import io.vertx.core.json.JsonObject;
 import io.vertx.rxjava3.core.AbstractVerticle;
+import io.vertx.rxjava3.ext.mongo.MongoClient;
 
 public class MainVerticle extends AbstractVerticle {
 
   @Override
   public void start(Promise<Void> startPromise) throws Exception {
+    getMongoClient("mongodb://mongo:mongo@mongo:27017/security?authSource=admin");
     vertx.createHttpServer().requestHandler(req -> {
       req.response().putHeader("content-type", "text/plain").end("Hello from Vert.x!");
     }).listen(5000).subscribe(server -> {
@@ -15,5 +21,23 @@ public class MainVerticle extends AbstractVerticle {
     }, error -> {
       startPromise.fail(error);
     });
+  }
+
+  private MongoClient getMongoClient(String connectionString) {
+    MongoClient mongoClient = MongoClient.create(vertx, new JsonObject()//
+        .put("connection_string", connectionString));
+
+    mongoClient.rxGetCollections().subscribe(collectionList -> {
+      List<Completable> createCollection = new ArrayList<>();
+      if (!collectionList.contains("users")) {
+        createCollection.add(mongoClient.rxCreateCollection("users"));
+      }
+
+      if (!createCollection.isEmpty()) {
+        Completable.merge(createCollection).subscribe();
+      }
+    });
+
+    return mongoClient;
   }
 }

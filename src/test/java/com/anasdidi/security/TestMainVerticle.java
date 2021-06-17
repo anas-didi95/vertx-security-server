3c5138927e453a4538ac9e636e26ac47bf84fe2a
Anas Juwaidi <anas.didi95@gmail.com>
2021-06-17 15:58:30 +0800

    Add test

--- src/test/java/com/anasdidi/security/TestMainVerticle.java
+++ src/test/java/com/anasdidi/security/TestMainVerticle.java
@@ -4,9 +4,11 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import io.vertx.core.json.JsonObject;
 import io.vertx.junit5.VertxExtension;
 import io.vertx.junit5.VertxTestContext;
 import io.vertx.rxjava3.core.Vertx;
+import io.vertx.rxjava3.ext.mongo.MongoClient;
 
 @ExtendWith(VertxExtension.class)
 public class TestMainVerticle {
@@ -24,5 +26,20 @@ public class TestMainVerticle {
   @Test
   void testVerticleDeployed(Vertx vertx, VertxTestContext testContext) throws Throwable {
     testContext.completeNow();
+  }
+
+  @Test
+  void testMongoClientGetCollectionsSuccess(Vertx vertx, VertxTestContext testContext) {
+    MongoClient mongoClient = MongoClient.create(vertx, new JsonObject()//
+        .put("connection_string", "mongodb://mongo:mongo@mongo:27017/security?authSource=admin"));
+
+    mongoClient.rxGetCollections().subscribe(collectionList -> {
+      testContext.verify(() -> {
+        Assertions.assertTrue(!collectionList.isEmpty());
+        testContext.completeNow();
+      });
+    }, error -> {
+      testContext.failNow(error);
+    });
   }
 }

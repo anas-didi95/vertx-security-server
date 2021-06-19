60aeeee1a119f873815f6d68f8f50a869014346c
Anas Juwaidi <anas.didi95@gmail.com>
2021-06-19 21:04:13 +0800

    Add test for user created in database

--- src/test/java/com/anasdidi/security/domain/user/TestUserVerticle.java
+++ src/test/java/com/anasdidi/security/domain/user/TestUserVerticle.java
@@ -12,6 +12,7 @@ import io.vertx.junit5.Checkpoint;
 import io.vertx.junit5.VertxExtension;
 import io.vertx.junit5.VertxTestContext;
 import io.vertx.rxjava3.core.Vertx;
+import io.vertx.rxjava3.ext.mongo.MongoClient;
 import io.vertx.rxjava3.ext.web.client.WebClient;
 
 @ExtendWith(VertxExtension.class)
@@ -29,9 +30,11 @@ public class TestUserVerticle {
 
   @Test
   void testUserCreateSuccess(Vertx vertx, VertxTestContext testContext) {
-    Checkpoint checkpoint = testContext.checkpoint(2);
+    Checkpoint checkpoint = testContext.checkpoint(3);
     ApplicationConfig config = ApplicationConfig.instance();
     WebClient webClient = WebClient.create(vertx);
+    MongoClient mongoClient = TestUtils.getMongoClient(vertx, config.getMongoConnectionString());
+
     String suffix = ":" + System.currentTimeMillis();
     JsonObject requestBody = new JsonObject().put("username", "username" + suffix);
 
@@ -49,6 +52,17 @@ public class TestUserVerticle {
             Assertions.assertNotNull(responseBody.getString("id"));
             checkpoint.flag();
           });
+
+          String id = response.bodyAsJsonObject().getString("id");
+          JsonObject query = new JsonObject().put("_id", id);
+          JsonObject fields = new JsonObject();
+          mongoClient.findOne("users", query, fields).toSingle().subscribe(result -> {
+            testContext.verify(() -> {
+              Assertions.assertEquals(requestBody.getString("username"),
+                  result.getString("username"));
+              checkpoint.flag();
+            });
+          }, error -> testContext.failNow(error));
         }, error -> testContext.failNow(error));
   }
 }

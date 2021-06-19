68c39969b7e5ee9b8132ac4bad55e9e1dd46133c
Anas Juwaidi <anas.didi95@gmail.com>
2021-06-19 20:54:05 +0800

    Implement send response body

--- src/main/java/com/anasdidi/security/domain/user/UserHandler.java
+++ src/main/java/com/anasdidi/security/domain/user/UserHandler.java
@@ -1,11 +1,22 @@
 package com.anasdidi.security.domain.user;
 
+import java.util.HashMap;
+import java.util.Map;
+import io.reactivex.rxjava3.core.Single;
+import io.vertx.core.json.JsonObject;
 import io.vertx.rxjava3.ext.web.RoutingContext;
 
 class UserHandler {
 
   void create(RoutingContext routingContext) {
-    routingContext.response().putHeader("Content-Type", "application/json").setStatusCode(201)
-        .end();
+    Single<JsonObject> subscriber =
+        Single.just(new JsonObject().put("id", System.currentTimeMillis()));
+
+    subscriber.subscribe(responseBody -> {
+      Map<String, String> headers = new HashMap<>();
+      headers.put("Content-Type", "application/json");
+      routingContext.response().setStatusCode(201).headers().addAll(headers);
+      routingContext.response().end(responseBody.encode());
+    });
   }
 }

9fb36c1919a47643ec8ef64c1f4c541ed8836348
Anas Juwaidi <anas.didi95@gmail.com>
2021-07-09 16:12:18 +0800

    Amend command to drop collection to start fresh

--- mongo_migration/V1.0.0.js
+++ mongo_migration/V1.0.0.js
@@ -2,7 +2,7 @@
 // https://docs.mongodb.com/manual/reference/method/db.collection.createIndex/
 // https://docs.mongodb.com/manual/reference/method/db.collection.updateOne/#mongodb-method-db.collection.updateOne
 
-db.createCollection("users");
+db.users.drop();
 db.users.createIndex({ username: 1 }, { name: "uq_username", unique: true });
 
 // DO NOT REMOVE! This line will keep track migration file.

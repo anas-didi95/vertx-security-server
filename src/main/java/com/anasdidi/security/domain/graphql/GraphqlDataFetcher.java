e696127488f2cd1cb0b428e8e1e5ec33a69379cb
Anas Juwaidi <anas.didi95@gmail.com>
2021-07-22 20:51:20 +0800

    Add logger

--- src/main/java/com/anasdidi/security/domain/graphql/GraphqlDataFetcher.java
+++ src/main/java/com/anasdidi/security/domain/graphql/GraphqlDataFetcher.java
@@ -2,19 +2,23 @@ package com.anasdidi.security.domain.graphql;
 
 import java.util.HashMap;
 import java.util.Map;
+import com.anasdidi.security.common.ApplicationUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import graphql.schema.DataFetchingEnvironment;
 import io.vertx.core.Promise;
 
 class GraphqlDataFetcher {
 
+  private static final Logger logger = LogManager.getLogger(GraphqlDataFetcher.class);
+
   void ping(DataFetchingEnvironment env, Promise<Map<String, Object>> promise) {
-    // final String TAG = "ping";
-    // String requestId = CommonUtils.generateUUID(env.getExecutionId());
+    String traceId = ApplicationUtils.getFormattedUUID(env.getExecutionId().toString());
     String value = env.getArgument("value");
 
-    // if (logger.isDebugEnabled()) {
-    // logger.debug("[{}:{}] value={}", TAG, requestId, value);
-    // }
+    if (logger.isDebugEnabled()) {
+      logger.debug("[ping:{}] value={}", traceId, value);
+    }
 
     Map<String, Object> result = new HashMap<>();
     result.put("isSuccess", true);

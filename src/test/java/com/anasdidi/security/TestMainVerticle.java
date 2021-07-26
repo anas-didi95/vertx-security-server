c209f1fca7e7d8d7065501d6e36574ff90281169
Anas Juwaidi <anas.didi95@gmail.com>
2021-07-26 20:19:00 +0800

    Add test for endpoint

--- src/test/java/com/anasdidi/security/TestMainVerticle.java
+++ src/test/java/com/anasdidi/security/TestMainVerticle.java
@@ -1,11 +1,14 @@
 package com.anasdidi.security;
 
 import com.anasdidi.security.common.ApplicationConfig;
+import com.anasdidi.security.common.ApplicationConstants;
 import com.anasdidi.security.common.TestUtils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import io.vertx.core.json.JsonObject;
+import io.vertx.junit5.Checkpoint;
 import io.vertx.junit5.VertxExtension;
 import io.vertx.junit5.VertxTestContext;
 import io.vertx.rxjava3.core.Vertx;
@@ -13,6 +16,8 @@ import io.vertx.rxjava3.ext.mongo.MongoClient;
 
 @ExtendWith(VertxExtension.class)
 public class TestMainVerticle {
+
+  private final String baseURI = ApplicationConstants.CONTEXT_PATH;
 
   @BeforeEach
   void deployVerticle(Vertx vertx, VertxTestContext testContext) {
@@ -56,5 +61,25 @@ public class TestMainVerticle {
     Assertions.assertNotNull(config.getJwtRefreshTokenExpireInMinutes());
     Assertions.assertNotNull(config.getGraphiqlEnable());
     testContext.completeNow();
+  }
+
+  @Test
+  void testHealthEndpointSuccess(Vertx vertx, VertxTestContext testContext) {
+    Checkpoint checkpoint = testContext.checkpoint();
+
+    TestUtils.doGetRequest(vertx, TestUtils.getRequestURI(baseURI, "health")).rxSend()
+        .subscribe(response -> {
+          testContext.verify(() -> {
+            Assertions.assertEquals(200, response.statusCode());
+            checkpoint.flag();
+          });
+
+          testContext.verify(() -> {
+            JsonObject responseBody = response.bodyAsJsonObject();
+            Assertions.assertNotNull(responseBody);
+            Assertions.assertEquals("UP", responseBody.getString("outcome"));
+            checkpoint.flag();
+          });
+        }, error -> testContext.failNow(error));
   }
 }

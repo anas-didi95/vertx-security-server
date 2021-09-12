74c4e431416e266855c0b961563477e727022170
Anas Juwaidi <anas.didi95@gmail.com>
2021-09-12 20:29:21 +0800

    ADD: Add new value for permission collection migration

--- mongo_migration/V1.2.0.js
+++ mongo_migration/V1.2.0.js
@@ -3,7 +3,8 @@
 // https://docs.mongodb.com/manual/reference/method/db.collection.updateOne/#mongodb-method-db.collection.updateOne
 
 db.permissions.drop();
-db.permissions.insert({ "_id": "user:write" });
+db.permissions.insert({ "_id": "security:user" });
+db.permissions.insert({ "_id": "security:graphql" });
 
 // DO NOT REMOVE! This line will keep track migration file.
 db.migration.updateOne({ database: "security" }, { "$set": { version: "1.2.0" } }, { upsert: true });
